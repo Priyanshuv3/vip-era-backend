006f813af7951d950ad5acb384fe1d552d5a75eb
priyanshu verma <priyanshuv3@gmail.com>
2025-06-24 22:18:48 +0530

    whitenoise sequence updated

--- vipera/vipera/settings.py
+++ vipera/vipera/settings.py
@@ -58,8 +58,8 @@ AUTHENTICATION_BACKENDS = [
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
